0a6e9a519c1d5ea4345d816d9f6b010216633140
Vlad <vladribak24@gmail.com>
2025-03-24 10:22:20 +0000

    added user-login form

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', include('main.urls'), name='index'),
     path('users/', include('users.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
