ec95017445e9d5e22998d1affec3260984659399
Vlad <vladribak24@gmail.com>
2025-04-01 09:47:22 +0100

    decorated buttons

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout'),
 
 ]
